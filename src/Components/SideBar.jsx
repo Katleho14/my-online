596e9af2208ea460ea62c7eb021385ab20a3eddf
Katleho14 <katlehomotloung394@gmail.com>
2024-11-25 20:31:49 +0200

    "Updated .vscode/settings.json and refactored SearchBar.jsx to SideBar.jsx, changing its functionality and adding new assets and links."

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,34 +1,54 @@
 import React, { useState } from "react";
-import "../styles/SearchBar.css";
+import '../styles/Sidebar.css';
+import logoImage from '../Assets/logoImage.png';
+import  menuIcon from '../Assets/menuIcon.png';
+import storeIcon from '../Assets/storeIcon.png';
+import cartbag from '../Assets/cartbag.png';
+import logoutIcon from '../Assets/logoutIcon.png';
+import { Link } from "react-router-dom";
 
-function SearchBar({ onSearch }) {
-    const [searchQuery, setSearchQuery] = useState("");
+function SideBar() {
+    const [isExpanded, setIsExpanded] = useState(false);
 
-    const handleInputChange = (event) => {
-        const query = event.target.value;
-        setSearchQuery(query);
-       onSearch(query);
+    const toggleSidebar = () => {
+        setIsExpanded(!isExpanded);
     };
 
-   
-
     return (
-        <div className="dashboard">
-            <div className="top-bar">
-                <div className="label-1">
-                    <span className="label">Search Item</span>
+        <div className={`navbar ${isExpanded ? 'expanded' : 'collapsed'}`}>
+            <div className="nav-links-top">
+                <div className="logo">
+                    <img className="group-1" src={logoImage} alt="Logo" />
                 </div>
-                <div className="input-field">
-                    <input
-                        type="text"
-                        placeholder="Apple Watch, Samsung S21, Macbook Pro, ..."
-                        value={searchQuery}
-                        onChange={handleInputChange}
-                    />
+                <div className="menu-icon" onClick={toggleSidebar}>
+                    <div className="icon">
+                        <img className="vector-4" src={menuIcon} alt="Menu Icon" />
+                    </div>
                 </div>
+                <div className="nav-links">
+                    <Link to="/" className="nav-link">
+                      
+                        <img className="vector-2" src={storeIcon} alt="Store" />
+                      
+                        {isExpanded && <span className="nav-text">Store</span>}
+                        </Link> 
+                   
+                    <Link to="/cart" className="nav-link">
+                    
+                    <img className="vector-3" src={cartbag} alt="Cart" />
+                    
+                        {isExpanded && <span className="nav-text">Cart</span>}
+                        </Link> 
+                </div>
+            </div>
+            <div className="nav-link-bottom">
+                <Link to="/" className="nav-link-2">
+                    <img className="vector-5" src={logoutIcon} alt="Nav Link 3" />
+                    {isExpanded && <span className="nav-text">LogOut</span>}
+                </Link>
             </div>
         </div>
     );
 }
 
-export default SearchBar;
+export default SideBar;
